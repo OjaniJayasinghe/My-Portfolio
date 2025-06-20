e36373fe896f629a9bcdf52cdc0f6bccf3e2bc70
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-06-20 17:19:36 +0530

    changed file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -65,6 +65,7 @@ const styles = {
     padding: "1.2rem 2.5rem",
     fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
     boxShadow: "0 3px 10px rgba(0, 0, 0, 0.4)",
+    width: "auto",
     zIndex: 900,
   },
   titleGradient: {
